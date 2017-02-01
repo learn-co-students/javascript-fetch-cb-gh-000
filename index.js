75dc53ea25f5cf9d65a87554760fd67a148b4851
Zachari LaCombe <zacharilouis@gmail.com>
2017-02-01 10:33:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const app = "I don't do much.";
+const fetch = require('isomorphic-fetch')
+
+const token = "bf97e6ba479de69c3f7bd7cbd148211a4ab0605e";
+const init = {
+  headers: {
+    Authorization: `token ${token}`
+  }
+};
+
+fetch('https://api.github.com/user/repos', init)
+  .then(res => res.json())
+  .then(json => console.log(json));
