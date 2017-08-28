8aed68c657e55e9dfab626a220085dd586eeb67b
Jhondee Diaz <jhondeediaz@gmail.com>
2017-08-28 02:03:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+$(document).ready(function(){
+  const token = '70ce63cf404be6ec89dc9af349b39d55e77e9a63'
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
+})
