4d0d6ded6e4983f8f5e20304ae5d4b7293ee565a
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-16 05:33:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const app = "I don't do much.";
+const token = "d0a3d956ad0adcb7bde4fb6733fbe89aaef50d58";
+fetch("https://api.github.com/user/repos", {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
