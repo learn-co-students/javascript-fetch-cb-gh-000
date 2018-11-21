21add29d17311da9d340c04b18be0c820a48039c
Susanta Biswas <susantasky@gmail.com>
2018-11-21 06:56:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+const token = '';
+
+function showRepo(){
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json())
+  .then(json => console.log(json));
+
+}
