76d8561ef00f2faa750c430eee253d37527c6d64
Recep Ekşi <portre_90@hotmail.com>
2020-01-06 12:27:13 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-const app = "I don't do much.";
+const token = '8df4b0c593f84e81d261558bf29731506a8873a2';
+asd=fetch('https://api.github.com/AndylLow/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
+alert(asd);
