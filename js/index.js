8e1631e7d54d9a96df9d3f91bc35a68520dd2837
Armintas Zadeika <az778@york.ac.uk>
2020-05-22 13:04:33 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much.";
+const token = '59f2bafb5def3159366ccfa6c97ac8e224234ab4';
+const url = 'https://api.github.com/user/repos';
+fetch(url, {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
